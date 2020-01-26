8c49ae1234df3c2470868a82f000424cce5c08a5
Anvesh-Babu <59701024+Anvesh-Babu@users.noreply.github.com>
2020-01-26 17:10:47 +0530

    Add files via upload

---
+++ src/ex3/exmp1.java
@@ -0,0 +1,21 @@
+package ex3;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class exmp1 {
+
+	public static void main(String[] args) {
+		String str = null;
+		Scanner sc = new Scanner(System.in);
+		
+		str = sc.nextLine();
+		String[] arrOfStr = str.trim().split("\\s+"); 
+		for(int i=0;i<arrOfStr.length;i++) {
+			System.out.println(arrOfStr[i]);
+		}
+		System.out.println("output string: " + Arrays.toString(arrOfStr));
+ 	}
+
+}
+
+
